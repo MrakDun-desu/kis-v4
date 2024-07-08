6bfa4fc0400e4efab962e6a2cd8028d25185b79b
MrakDundesu <m.danco@protonmail.com>
2024-07-08 02:36:37 +0200

    Initial commit

---
+++ Api/Program.cs
@@ -0,0 +1,44 @@
+var builder = WebApplication.CreateBuilder(args);
+
+ConfigureCors(builder.Services);
+ConfigureOpenApiDocuments(builder.Services);
+
+var app = builder.Build();
+
+app.UseCors();
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseSwagger();
+app.UseSwaggerUI();
+UseDevelopmentSettings(app);
+UseEndpoints(app);
+
+app.Run();
+
+return;
+
+void ConfigureCors(IServiceCollection serviceCollection) {
+    serviceCollection.AddCors(opts => {
+        opts.AddDefaultPolicy(policy =>
+            policy.AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod());
+    });
+}
+
+void ConfigureOpenApiDocuments(IServiceCollection serviceCollection) {
+    serviceCollection.AddEndpointsApiExplorer();
+    serviceCollection.AddSwaggerGen();
+}
+
+void UseDevelopmentSettings(WebApplication application) {
+    var environment = application.Services.GetRequiredService<IWebHostEnvironment>();
+
+    if (environment.IsDevelopment()) {
+        application.UseDeveloperExceptionPage();
+    }
+}
+
+void UseEndpoints(IEndpointRouteBuilder routeBuilder) {
+    routeBuilder.MapGet("hello-world", () => "Hello, world!");
+}
