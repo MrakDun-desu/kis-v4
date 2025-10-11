d356151360ba47da6c38b729dd63750a669f7268
MrakDundesu <m.danco@protonmail.com>
2025-10-11 13:46:55 +0300

    Add Dockerfile for frontend

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -6,12 +6,12 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'src/api-generated']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.stylisticTypeChecked,
+      tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
