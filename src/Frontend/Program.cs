2da949844884e667fb46c8830d692ae1743e3f05
MrakDundesu <m.danco@protonmail.com>
2025-11-15 13:41:33 +0200

    Move frontend code into the .NET solution

---
+++ src/Frontend/Program.cs
@@ -0,0 +1,44 @@
+using Duende.Bff.Yarp;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthorization();
+
+builder.Services
+    .AddBff()
+    .AddRemoteApis();
+
+builder.Services
+    .AddAuthentication(options => {
+        options.DefaultScheme = "Cookies";
+        options.DefaultChallengeScheme = "oidc";
+        options.DefaultSignOutScheme = "oidc";
+    })
+    .AddCookie("Cookies")
+    .AddOpenIdConnect("oidc", options => {
+        options.Authority = "https://localhost:5001";
+        options.ClientId = "bff";
+        options.ClientSecret = "secret";
+        options.ResponseType = "code";
+        options.Scope.Add("api1");
+        options.Scope.Add("offline_access");
+        options.SaveTokens = true;
+        options.GetClaimsFromUserInfoEndpoint = true;
+        options.MapInboundClaims = false;
+    });
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment()) {
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthentication();
+app.UseBff();
+app.UseAuthorization();
+app.MapBffManagementEndpoints();
+
+app.Run();
--- src/Frontend/ViteApp/vite.config.ts
+++ src/Frontend/ViteApp/vite.config.ts
@@ -3,6 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/bff": "https::/localhost:5001",
+      "/signin-oidc": "https::/localhost:5001",
+      "/signout": "https::/localhost:5001",
+      "/api": "https::/localhost:5001",
+    }
+  },
+  build: {
+    outDir: "../wwwroot"
+  },
   plugins: [
     react({
       babel: {
